118ff561104f13939aebaac6017b6974356aa53d
Steve Hebert <steve.hebert@dowjones.com>
2016-08-18 07:55:35 -0500

    adding sample app with 0mq

--- src/cqrs-test/Class1.cs
+++ src/cqrs-test/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using cqrs.Messaging;
+using NetMQ;
+using NetMQ.Sockets;
 using Xunit;
 
 namespace cqrs_test
@@ -8,11 +10,25 @@ namespace cqrs_test
         [Fact]
         public void Go()
         {
-            int i = 5;
-            var e = new Envelope<int>(i);
+            using (var server = new ResponseSocket("@tcp://localhost:5556")) // bind
+            using (var client = new RequestSocket(">tcp://localhost:5556"))  // connect
+            {
+                // Send a message from the client socket
+                client.SendFrame("Hello");
 
-            Assert.Equal(e.Body, i);
+                // Receive the message from the server socket
+                string m1 = server.ReceiveFrameString();
+                Assert.Equal(m1, "Hello");
 
+
+                // Send a response back from the server
+                server.SendFrame("Hi Back");
+
+                // Receive the response from the client socket
+                string m2 = client.ReceiveFrameString();
+                Assert.Equal(m2, "Hi Back");
+
+            }
         }
     }
 }
