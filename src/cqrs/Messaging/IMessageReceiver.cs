ee1f8ae8cf954b56a29ecf1117142dad8ffbd9ff
Steve Hebert <steve.hebert@gmail.com>
2016-08-23 05:57:08 -0500

    added message processing

---
+++ src/cqrs/Messaging/IMessageReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace cqrs.Messaging
+{
+    public interface IMessageReceiver<T>
+    {
+        void Start(Func<T, MessageReleaseAction> messageHandler);
+
+        void Stop();
+    }
+}
---
+++ src/cqrs/Messaging/MessageReleaseAction.cs
@@ -0,0 +1,31 @@
+﻿namespace cqrs.Messaging
+{
+    public class MessageReleaseAction
+    {
+        public static readonly MessageReleaseAction CompleteMessage = new MessageReleaseAction(MessageReleaseActionKind.Complete);
+        public static readonly MessageReleaseAction AbandonMessage = new MessageReleaseAction(MessageReleaseActionKind.Abandon);
+
+        protected MessageReleaseAction(MessageReleaseActionKind kind)
+        {
+            this.Kind = kind;
+        }
+
+        public MessageReleaseActionKind Kind { get; private set; }
+
+        public string DeadLetterReason { get; private set; }
+
+        public string DeadLetterDescription { get; private set; }
+
+        public static MessageReleaseAction DeadLetterMessage(string reason, string description)
+        {
+            return new MessageReleaseAction(MessageReleaseActionKind.DeadLetter) { DeadLetterReason = reason, DeadLetterDescription = description };
+        }
+    }
+
+    public enum MessageReleaseActionKind
+    {
+        Complete,
+        Abandon,
+        DeadLetter
+    }
+}
